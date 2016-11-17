60324da68e9c8b406c95fd2144381c4dfede4efe
Balaban Mihai <Balaban Mihai>
2016-11-17 03:21:32 +0200

    home page

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,52 +2,204 @@
 
 /* @var $this yii\web\View */
 
+use yii\bootstrap\Carousel;
+
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,700,300' rel='stylesheet' type='text/css'>
+<link
+    href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic'
+    rel='stylesheet' type='text/css'>
+<link href="https://file.myfontastic.com/oAnRr49LRSuj7cYjEYP7b9/icons.css" rel="stylesheet">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<!-- Navigation -->
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+<!-- Hero section -->
+<div class="container-fluid hero-container">
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+    <!--    <div class="container">-->
+    <!--        <div class="row hero-row">-->
+    <!---->
+    <!--            <div class="col-md-6 hero-text">-->
+    <!--                <h1 class="hero-title">Bine ati vanit la Clinica...</h1>-->
+    <!---->
+    <!--                <h3 class="hero-under-title">Aici va fi o descriere a clinicii</h3>-->
+    <!--                <button type="button" class="btn btn-lg btn-hero">Citeste mai mult</button>-->
+    <!--                <button type="button" class="btn btn-lg btn-hero">Servicii oferite</button>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="col-md-6">-->
+    <!--                <img class="hero-image" src="http://www.pngmart.com/files/3/White-Teeth-PNG-Clipart.png">-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
+    <?php
+    $images = ['<img src="http://www.pngmart.com/files/3/White-Teeth-PNG-Clipart.png"/>',
+        '<img src="http://www.pngmart.com/files/3/White-Teeth-PNG-Clipart.png"/>',
+        '<img src="http://www.pngmart.com/files/3/White-Teeth-PNG-Clipart.png"/>'];
+    echo yii\bootstrap\Carousel::widget(['items' => $images]);
+    ?>
 
-    <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+</div>
+<!-- Hero section -->
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+<!-- Services section -->
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+<div class="container services">
+
+    <div class="row">
+
+        <div class="col-lg-4">
+
+            <div class="icon-list-item">
+
+                <div class="icon-list-icon">
+                    <span class="icon icon-toothbraces"></span>
+                </div>
+
+                <div class="icon-list-content">
+                    <h3 class="icon-list-title">Extractii 1 </h3>
+                    <h5 class="icon-list-under">Lorem ipsum.</h5>
+                </div>
+
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+        </div>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+        <div class="col-lg-4">
+
+            <div class="icon-list-item">
+
+                <div class="icon-list-icon">
+                    <span class="icon icon-toothstop"></span>
+                </div>
+
+                <div class="icon-list-content">
+                    <h3 class="icon-list-title">Extractii 2</h3>
+                    <h5 class="icon-list-under">Lorem ipsum</h5>
+                </div>
+
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+        </div>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        <div class="col-lg-4">
+
+            <div class="icon-list-item">
+
+                <div class="icon-list-icon">
+                    <span class="icon icon-toothimplant"></span>
+                </div>
+
+                <div class="icon-list-content">
+                    <h3 class="icon-list-title">Extractii 3</h3>
+                    <h5 class="icon-list-under">Another lorem ipsum.</h5>
+                </div>
+
             </div>
+
         </div>
 
     </div>
+
+    <div class="row">
+
+        <div class="col-lg-4">
+
+            <div class="icon-list-item">
+
+                <div class="icon-list-icon">
+                    <span class="icon icon-toothcheck"></span>
+                </div>
+
+                <div class="icon-list-content">
+                    <h3 class="icon-list-title">Extractii 4</h3>
+                    <h5 class="icon-list-under">this time is a bigger lorem ipsum.</h5>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-4">
+
+            <div class="icon-list-item">
+
+                <div class="icon-list-icon">
+                    <span class="icon icon-toothsparkle"></span>
+                </div>
+
+                <div class="icon-list-content">
+                    <h3 class="icon-list-title">Extractii 5</h3>
+                    <h5 class="icon-list-under">it si 3 o'clock</h5>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-4">
+
+            <div class="icon-list-item">
+
+                <div class="icon-list-icon">
+                    <span class="icon icon-toothcrack"></span>
+                </div>
+
+                <div class="icon-list-content">
+                    <h3 class="icon-list-title">Extractii 6</h3>
+                    <h5 class="icon-list-under">And the last one for tonight</h5>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Services section End -->
+
+<div class="container-fluid contact-section">
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-5">
+
+                <h2 class="contact-form-title">Contactare</h2>
+
+                <div class="testimonial">
+                    <p class="testimonial-text">"Aici va fi un text lung de descriere"</p>
+                    <h4 class="testimonial-name">Jhon Doe, Pacient</h4>
+                </div>
+                <div class="testimonial">
+                    <p class="testimonial-text">Aici va fi alt text lung de descriere a vaccinului</p>
+                    <h4 class="testimonial-name">Donald Trumo, Pacient</h4>
+                </div>
+
+            </div>
+
+            <div class="col-lg-6">
+                <h2 class="contact-form-title">Programeaza-te</h2>
+                <ul class="input-list style-1 clearfix">
+                    <li>
+                        <input type="text" placeholder="Please enter your name">
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Enter your email">
+                    </li>
+                    <li>
+                        <input type="text" placeholder="Enter your phone number">
+                    </li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-hero contact-button">Trimite</button>
+            </div>
+
+        </div>
+    </div>
+
 </div>
